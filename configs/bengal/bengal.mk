ffca9ee3a4254e9c17cf62e6babf4130be79d2b0
Subhadra Jagadeesan <quic_c_subhja@quicinc.com>
2022-09-06 15:35:41 +0530

    configs: bengal: Rename mixer xml for bengal qrd
    1.Rename the mixer file and resource manager xml file for
    qrd variant.
    2. Updated amic1, amic2 and amic3 mixer controls for record.
    3. Added va mic mono mixer controls for VoiceUI.
    
    Change-Id: I253545fce5169570b15fac343660cbc22bb90a1a
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- configs/bengal/bengal.mk
+++ configs/bengal/bengal.mk
@@ -183,7 +183,7 @@ PRODUCT_COPY_FILES += \
     $(TARGET_HALS_PATH)/audio/configs/bengal/mixer_paths_bengal_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_bengal_idp.xml \
     $(TARGET_HALS_PATH)/audio/configs/bengal/mixer_paths_scubaidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_scubaidp.xml \
     $(TARGET_HALS_PATH)/audio/configs/bengal/mixer_paths_scubaqrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_scubaqrd.xml \
-    $(TARGET_HALS_PATH)/audio/configs/bengal/mixer_paths_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd.xml \
+    $(TARGET_HALS_PATH)/audio/configs/bengal/mixer_paths_bengal_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_bengal_qrd.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
 
